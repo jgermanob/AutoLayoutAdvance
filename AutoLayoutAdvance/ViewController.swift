32f49b06f505a522a3de1434073682b77e712fce
Jesús Germán Ortiz Barajas D3 <German_Diplomado@macbook-13.local>
2019-03-29 17:03:47 -0600

    Initial Commit

---
+++ AutoLayoutAdvance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutAdvance
+//
+//  Created by Jesús Germán Ortiz Barajas D3 on 3/29/19.
+//  Copyright © 2019 germanco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
